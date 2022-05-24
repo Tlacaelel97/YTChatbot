3f006d8150543ef7a141e4e5b020f249bf0789c8
Tlacaelel Flores <tlacaelel.flores@ciencias.unam.mx>
2022-05-24 18:33:17 -0500

    Join/leave voice

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ async def hello(ctx):
 async def goodbye(ctx):
     await ctx.send("Goodbye, have a nice day!")
 
+"""
+Welcome a New member
+"""
+
 @client.event
 #when a user enter the server
 async def on_member_join(member):
@@ -46,10 +50,36 @@ async def on_member_join(member):
     await channel.send('Hello and welcome to the server! Here is a joke:')
     await channel.send(json.loads(response.text)['content'])
 
+"""
+Goodbye to a membr
+"""
 @client.event
 async def on_member_remove(member):
     channel = client.get_channel(770542136160157740)
     await channel.send('Goodbye')
+
+"""
+Joining and leaving a channel voice
+"""
+
+@client.command(pass_context= True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send('You must be in a voice channel to run this command')
+
+@client.command(pass_context= True)
+async def leave(ctx):
+    #If the bot is in a voiice channel
+    if (ctx .voice_client):
+        #Then leave the channel
+        await ctx.guild.voice_client.disconnect()
+        #Say goodbye
+        await ctx.send('I left the voice channel')
+    else:
+        await ctx.send('I am not in a voice channel')
 
 client.run(BOTTOKEN) 
 
